1a59c47fd2b48de20dcfc6c426a6b1109b5aba25
Josuegs <jgalarza097@gmail.com>
2020-10-15 00:40:40 -0500

    proyect initial

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const numbers = [1,2,3,4]
+const numbersDouble = numbers.map(number => number*2)
+console.log(numbers)
+
+class Person{
+
+  constructor(name){
+    this.name = name
+  }
+}
---
+++ src/js/second.js
@@ -0,0 +1,3 @@
+const numbers2 = [1,2,3,4,5,6]
+const numbersTriple = numbers.map(number => number*3)
+console.log('hello world');
